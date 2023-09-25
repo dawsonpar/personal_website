9fd1f2fa56431d586ea851424d1e8bea4f7aa2f6
Dawson Par <dawson@andrews.edu>
2023-09-24 23:50:55 -0400

    added andrews experience to experience section

--- components/home/experience/Experience.tsx
+++ components/home/experience/Experience.tsx
@@ -14,10 +14,19 @@ export const Experience = () => {
 
 const experience = [
   {
+    title: "Andrews University",
+    position: "Data Asset Manager",
+    time: "September 2023 - Present",
+    location: "Berrien Springs, Michigan",
+    description:
+      "Streamlining the conversion of raw photos to DNG from the communication department's Google drive. Currently working on automating the pushing of finished assets into our database and content management system.",
+    tech: ["Python", "Bash", "Google APIs", "Adobe DNG"],
+  },
+  {
     title: "Advent Share Ministries",
     position: "Full Stack Developer",
-    time: "June 2023 - Present",
-    location: "Remote, Texas",
+    time: "June 2023 - September 2023",
+    location: "Edinburg, Texas",
     description: `Built dynamic UI routes and components for reusability across the website's pages. Created and implemented SSG componenets that improved page rendering. Current DNS doesn't point to AWS hosting so here's the temporary link https://master.d3tgctb81d9jd4.amplifyapp.com`,
     tech: ["NextJS", "TypeScript", "Tailwind", "Git", "GitHub", "AWS Amplify"],
   },
